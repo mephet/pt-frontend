f20d969e6bc94e6fed43d0d297da14797a490bb1
Koh <724537@cognizant.com>
2019-05-24 17:59:43 +0800

    updated pid

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import { Tabs, Tab, Container } from 'react-bootstrap';
 import Home from './components/Home/Home';
 import JBHead from './components/JBHead/JBHead';
 import SprintInfo from './components/SprintInfo/SprintInfo';
+import Constants from './constants';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.projectId = 2345186;
+    this.projectId = Constants.PROJECT_ID;
     this.state = {
       sprintData: {},
       isReleaseTaggingEnabled: false,
